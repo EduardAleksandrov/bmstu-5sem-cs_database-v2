8a8c42e2d4c740a5d4864eee7720bc7d54a66ad6
Eduard Aleksandrov <eduard.ale@list.ru>
2024-10-21 21:37:11 +0300

    Change home view

--- frontend/db-front/src/views/HomeView.vue
+++ frontend/db-front/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/warehouse_logo.png">
+    <HelloWorld msg="Warehouse Order Management Software"/>
   </div>
 </template>
 
@@ -16,3 +16,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+img{
+  width: 200px;
+}
+</style>
